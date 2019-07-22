77ec523d67146d208eac59ae510605ae8ac8adad
zloper <makiavel@mail.ru>
2019-07-22 22:52:24 +0300

    add some test

---
+++ Test/Sagashiter_test.go
@@ -0,0 +1,68 @@
+package Test
+
+import (
+	"io/ioutil"
+	"os"
+	"strings"
+	"subChanger/Sagashiter"
+	"testing"
+	"time"
+)
+
+const assText = `Text
+Dialogue: 10,0:00:08.77,0:00:13.87,Default,,0,0,0,,Well well? \NAnd you call this test?`
+
+const srtText = `1
+00:01:34,719 --> 00:01:37,847
+Why read file? Why not string?
+00:01:39,345 --> 00:01:42,211
+Because I can!`
+
+func TestAss(t *testing.T) {
+	res, err := Reader("fl.ass", assText)
+	if err != nil {
+		t.Error(err)
+	}
+	if !strings.Contains(res, ":18.77,0:00:23.87,") {
+		t.Error("Wrong result, time not add")
+	}
+
+}
+
+func TestSrt(t *testing.T) {
+	res, err := Reader("fl.srt", srtText)
+	if err != nil {
+		t.Error(err)
+	}
+
+	if !strings.Contains(res, "01:49,345 --> 00:01:52,21") {
+		t.Error("Wrong result, time not add", res)
+	}
+
+}
+
+func Reader(flName, flContent string) (string, error) {
+	err := ioutil.WriteFile(flName, []byte(flContent), os.ModePerm)
+	if err != nil {
+		return "", err
+	}
+
+	content, err := ioutil.ReadFile(flName)
+	if err != nil {
+		return "", err
+	}
+
+	strContent := string(content)
+
+	var obj Sagashiter.SagashiterInterface
+	if strings.HasSuffix(flName, ".ass") {
+		obj = Sagashiter.NewAssObj(strContent, flName)
+	} else {
+		obj = Sagashiter.NewSrtObj(strContent, flName)
+	}
+
+	timers := obj.Tansaku()
+	incTime := time.Duration(10) * time.Second
+	res := obj.IncreaseTime(timers, incTime)
+	return res, nil
+}
